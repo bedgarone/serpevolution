e5480bdbc97b86446b31ae300e2a5c1d3fa6e4c2
Bruno Oliveira <bedgar1@gmail.com>
2021-07-01 16:29:15 +0100

    finish google elements and fill patterns

--- components/elementpatterns.js
+++ components/elementpatterns.js
@@ -1,27 +1,31 @@
 import patterns from "../data/patterns";
 
 export default function ElementPatterns({ elm_patterns }) {
-  return (
-    <div className="d-flex flex-column">
-      {elm_patterns.map((element, key) => {
-        {
-          var pat = Object.entries(patterns).find((object) => {
-            return object[0] == element;
-          });
-        }
-        return (
-          <div
-            className="applied-pattern d-flex"
-            style={{
-              backgroundImage: pat[1].css_image,
-              backgroundSize: "cover",
-            }}
-            key={key}
-          >
-            {pat[1].name}
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (elm_patterns == []) {
+    return <p>There are no patterns applied to this element.</p>;
+  } else {
+    return (
+      <div className="d-flex flex-column">
+        {elm_patterns.map((element, key) => {
+          {
+            var pat = Object.entries(patterns).find((object) => {
+              return object[0] == element;
+            });
+          }
+          return (
+            <div
+              className="applied-pattern d-flex"
+              style={{
+                backgroundImage: pat[1].css_image,
+                backgroundSize: "cover",
+              }}
+              key={key}
+            >
+              {pat[1].name}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
--- data/patterns.js
+++ data/patterns.js
@@ -4,10 +4,50 @@ const patterns = {
     shortname: "mtabs",
     css_image: "url(../assets/patterns/mtabs.png)",
   },
+  bread: {
+    name: "Breadcrumbs",
+    shortname: "bread",
+    css_image: "url(../assets/patterns/bread.png)",
+  },
+  accord: {
+    name: "Accordion",
+    shortname: "accord",
+    css_image: "url(../assets/patterns/accord.png)",
+  },
+  carousel: {
+    name: "Carousel",
+    shortname: "carousel",
+    css_image: "url(../assets/patterns/carousel.png)",
+  },
+  cards: {
+    name: "Cards",
+    shortname: "cards",
+    css_image: "url(../assets/patterns/cards.png)",
+  },
+  disclose: {
+    name: "Progressive Disclosure",
+    shortname: "disclose",
+    css_image: "url(../assets/patterns/disclose.png)",
+  },
+  categ: {
+    name: "Categorization",
+    shortname: "categ",
+    css_image: "url(../assets/patterns/categ.png)",
+  },
   news: {
     name: "News Stream",
     shortname: "news",
     css_image: "url(../assets/patterns/news.png)",
+  },
+  thumb: {
+    name: "Thumbnail",
+    shortname: "thumb",
+    css_image: "url(../assets/patterns/thumb.png)",
+  },
+  grid: {
+    name: "Grid of Equals",
+    shortname: "grid",
+    css_image: "url(../assets/patterns/grid.png)",
   },
 };
 
