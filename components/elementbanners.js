efac22acbc3e291eabd2142ce6750ba6ee2b0b95
Bruno Oliveira <bedgar1@gmail.com>
2021-07-03 14:05:00 +0100

    elements list

--- components/elementbanners.js
+++ components/elementbanners.js
@@ -2,10 +2,20 @@ export default function ElementBanners({ elements }) {
   return (
     <div className="wrap-container">
       {elements.map((elm, key) => {
+        var elm_image =
+          "./assets/elements/" +
+          elm[1].engine +
+          "/" +
+          elm[1].shortname +
+          "/" +
+          elm[1].evolution[elm[1].evolution.length - 1];
         return (
-          <div className="id-box" key={key}>
-            <div className="id-code">{elm[1].name}</div>
-          </div>
+          <a href={"./elements/" + elm[1].shortname}>
+            <div className={"element-box " + elm[1].type} key={key}>
+              <div className="element-name">{elm[1].name}</div>
+              <img src={elm_image} />
+            </div>
+          </a>
         );
       })}
     </div>
